32027c22ee2fc8d22f99f2beb3975ef6c6ed0314
andrew <andrewtran@hey.com>
2021-12-03 18:27:01 -0600

    completed phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,48 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+// const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+
+// **TEST ROUTE**
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
+
+// **TEST ROUTE**
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// **TEST ROUTE**
+
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// **TEST ROUTE**
+
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
 
 module.exports = router;
